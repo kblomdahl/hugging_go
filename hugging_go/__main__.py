4d52d8c7a2ac4b3ce09e9d3184132d5c876301e4
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-22 23:29:58 +0000

    Add `train-model` command line argument

--- hugging_go/__main__.py
+++ hugging_go/__main__.py
@@ -1,12 +1,20 @@
 from .agent import Agent
 from .board_factory import BoardFactory
+from .dataset import load_sgf_files
 from .gtp import Gtp
-from .tokenizer import train_tokenizer
+from .model import train
+from .tokenizer import train_tokenizer, pretrained_tokenizer
 
 import sys
 
 if sys.argv[1] == 'train-tokenizer':
     train_tokenizer(files=sys.argv[2:])
+if sys.argv[1] == 'train-model':
+    dataset = load_sgf_files(sys.argv[2:])
+    train(
+        dataset,
+        tokenizer=pretrained_tokenizer()
+    )
 else:
     gtp = Gtp(
         agent=Agent(),
