b6cd20d216244d9ec1db232117517b93bdb073e3
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-23 01:18:21 +0000

    Add transformers support to the agent

--- hugging_go/agent.py
+++ hugging_go/agent.py
@@ -1,10 +1,26 @@
+from .vertex import Vertex
+
 class Agent:
+    def __init__(self, pipe):
+        self.pipe = pipe
+
     def play(self, board, color, vertex):
-        if not board.is_valid(color, vertex):
+        if not board.state.is_valid(color, vertex):
             return False
         else:
-            board.place(color, vertex)
+            board.state.place(color, vertex)
+            board.sequence.append(vertex.as_gtp())
             return True
 
     def genmove(self, board, color):
+        candidates = self.pipe(' '.join(board.sequence))
+
+        for cand in sorted(candidates[0], key=lambda c: c['score'], reverse=True):
+            vertex = Vertex(cand['label'])
+
+            if board.state.is_valid(color, vertex):
+                board.state.place(color, vertex)
+                board.sequence.append(vertex.as_gtp())
+                return vertex.as_gtp()
+
         return 'resign'
