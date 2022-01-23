9e40c6934883a0a3755fe82a101db80e8ea9fa49
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-23 01:18:21 +0000

    Add transformers support to the agent

--- hugging_go/board_factory.py
+++ hugging_go/board_factory.py
@@ -1,7 +1,14 @@
 from .board import Board
 
+from collections import namedtuple
+
+BoardWithHistory = namedtuple('BoardWithHistory', ['state', 'sequence'])
+
 class BoardFactory:
     def build(self, board_size, komi):
         assert board_size == 19, 'only 19×19 boards are supported'
 
-        return Board(komi)
+        return BoardWithHistory(
+            state=Board(komi),
+            sequence=[]
+        )
